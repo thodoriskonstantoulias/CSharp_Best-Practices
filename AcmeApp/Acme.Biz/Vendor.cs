9e82fa8b9abc646af2bbf02d0d502d8d5885fd9c
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-17 13:44:29 +0200

    How to write good methods and test them

--- AcmeApp/Acme.Biz/Vendor.cs
+++ AcmeApp/Acme.Biz/Vendor.cs
@@ -29,5 +29,47 @@ namespace Acme.Biz
                                                         this.Email);
             return confirmation;
         }
+        //How methods shold be written, below
+
+        /// <summary>
+        /// Sends a product order to the vendor
+        /// </summary>
+        /// <param name="product">Product to order</param>
+        /// <param name="quantity">Quantity of the product to order</param>
+        /// <returns></returns>
+        
+        //Typical method returns bool 
+        //If we want our method to return multiple fields we return OperationResult class which has more than one fields
+
+        //public bool PlaceOrder(Product product, int quantity)
+        public OperationResult PlaceOrder(Product product, int quantity)
+        {
+            //Defencive coding first
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+            if (quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+            }
+
+            //Body of method
+            var success = false;
+            var orderText = "Order from Acme Inc." + System.Environment.NewLine +
+                            "Product : " + product.ProductCode + System.Environment.NewLine +
+                            "Quantity : " + quantity;
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
+            if (confirmation.StartsWith("Message sent: "))
+            {
+                success = true;
+            }
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+
+
+        }
+
     }
 }
--- AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace Acme.Biz.Tests
 {
@@ -54,6 +55,42 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+        //Uncomment if you want the bool return 
+        //[TestMethod()]
+        //public void PlaceOrderTest()
+        //{
+        //    var vendor = new Vendor();
+        //    var product = new Product(1, "Saw,", "");
+        //    var expected = true;
+        //    var actual = vendor.PlaceOrder(product, 3);
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+        //[TestMethod()]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void PlaceOrderNullProduct()
+        //{
+        //    var vendor = new Vendor();
+        //    var actual = vendor.PlaceOrder(null, 3);
+        //}
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw,", "");
+            var expected = new OperationResult(true, "Order from Acme Inc.\r\nProduct : Tools-1\r\nQuantity : 3");
+            var actual = vendor.PlaceOrder(product, 3);
+
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrderNullProduct()
+        {
+            var vendor = new Vendor();
+            var actual = vendor.PlaceOrder(null, 3);
         }
     }
 }
